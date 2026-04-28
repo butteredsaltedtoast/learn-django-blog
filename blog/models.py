68013f90e4f8eac2e3a353108fd1f31ee12c54df
Omar Aziza <butteredsaltedtoast@gmail.com>
2026-04-27 22:18:25 -0400

    fixed time zone and added edit timestamp

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = 'comments')
