e02c8c04cf92c756fbae6edf83d9c107ff17cb11
Omar Aziza <butteredsaltedtoast@gmail.com>
2026-04-27 22:06:46 -0400

    add comments, edit, delete with author permissions

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,10 @@ from .oauth import ion_login, ion_callback, ion_logout
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('create/', views.post_create, name='post_create'),
-    path('ouath/login/', ion_login, name='ion_login'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('oauth/login/', ion_login, name='ion_login'),
     path('oauth/callback/', ion_callback, name='ion_callback'),
     path('oauth/logout/', ion_logout, name='ion_logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
@@ -21,3 +22,42 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = post.comments.all().order_by('-created_at')
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('ion_login')
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
+
+@login_required(login_url='/oauth/login/')
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.user != post.author:
+        return HttpResponseForbidden("You can only edit your own posts.")
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required(login_url='/oauth/login/')
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.user != post.author:
+        return HttpResponseForbidden("You can only delete your own posts.")
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
