c75c9c5adbe70a63ea359e522bdef92ecf094750
Omar Aziza <butteredsaltedtoast@gmail.com>
2026-04-27 21:45:11 -0400

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = 'comments')
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
